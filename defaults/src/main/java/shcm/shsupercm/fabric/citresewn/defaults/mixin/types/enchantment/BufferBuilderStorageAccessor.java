4c72079baa19fd5cd302c0d98c7baee9794701e5
NOOBNUBY <noobnuby77@gmail.com>
2024-01-28 16:07:55 +0900

    update 1.20.4

--- defaults/src/main/java/shcm/shsupercm/fabric/citresewn/defaults/mixin/types/enchantment/BufferBuilderStorageAccessor.java
+++ defaults/src/main/java/shcm/shsupercm/fabric/citresewn/defaults/mixin/types/enchantment/BufferBuilderStorageAccessor.java
@@ -4,12 +4,32 @@ import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.RenderLayer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import shcm.shsupercm.fabric.citresewn.defaults.cit.types.TypeEnchantment;
 
+import java.util.Map;
 import java.util.SortedMap;
 
 @Mixin(BufferBuilderStorage.class)
-public interface BufferBuilderStorageAccessor {
-    @Accessor
-    SortedMap<RenderLayer, BufferBuilder> getEntityBuilders();
+abstract class BufferBuilderStorageMixin implements TypeEnchantment.CITBufferBuilderStorage {
+    @Unique
+    private Map<RenderLayer, BufferBuilder> citresewn$entityBuilders;
+
+    @ModifyArg(
+            method = "<init>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/VertexConsumerProvider;immediate(Ljava/util/Map;Lnet/minecraft/client/render/BufferBuilder;)Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;"))
+    private Map<RenderLayer, BufferBuilder> citresewn$setEntityBuilders(Map<RenderLayer, BufferBuilder> original) {
+        citresewn$entityBuilders = original;
+        return original;
+    }
+
+    @Override
+    public Map<RenderLayer, BufferBuilder> citresewn$getEntityBuilders() {
+        return citresewn$entityBuilders;
+    }
 }
